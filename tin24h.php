b0e54a328f029feeb591d7f5d78c34595083a190
Kim Ngoc Huynh <knhuynhtctv@gmail.com>
2016-12-18 19:30:49 +0700

    [+] Code 18/12/2016 : Layout template trang tin24h. (phan 1)

---
+++ tin24h.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Xã Hội, Tin Tức Xã Hội</title>
+        <link href="css/all.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <?php
+            require '_include/topnav.php';
+            require '_include/banner.php';
+            // require '_include/mainnav.php';
+            // require '_include/topevent2.php';
+        ?>
+            <div class="wraper">
+                <div style="border-top: 3px solid #d52025"></div>
+                <div class="content-left">
+                    <div class="news-latest news-latest2">
+                        <div>
+                            <div class="title-24hours">
+                                Tin tức 24h
+                            </div>
+                            <div class="title-24hours2">
+                                <a>Tin tức 24h</a>
+                                <a>Tin mới</a>
+                                <a>Tin nhanh</a>
+                                <a>Tin nóng</a>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                        <div class="content-detail-left">
+                            <div class="block-24hours">
+                                <div class="" style="font-weight:bold; font-size:14px; color: #333; border-bottom: 1px solid #ccc">Kênh tin</div>
+                                <ul>
+                                   <li><a>Xã hội</a></li>
+                                   <li><a>Pháp luật</a></li>
+                                   <li><a>Thế giới</a></li>
+                                   <li><a>Giải trí</a></li>
+                                   <li><a>Kinh doanh</a></li>
+                                </ul>
+                                <div class="" style="font-weight:bold; font-size:14px; color: #333; border-bottom: 1px solid #ccc">Sự kiện</div>
+                                <ul>
+                                   <li><a>Xã hội</a></li>
+                                   <li><a>Pháp luật</a></li>
+                                   <li><a>Thế giới</a></li>
+                                   <li><a>Giải trí</a></li>
+                                   <li><a>Kinh doanh</a></li>
+                                </ul>
+                            </div>
+                            <img src="https://s-media-cache-ak0.pinimg.com/originals/5f/8d/9c/5f8d9c48f0a9079616b3ef1ee2fe299a.jpg">
+                        </div>
+                        <div class="content-detail-right content-detail-right-24hours">
+                            <div class="news-item">
+                                <div class="n-img"><img src="http://media.tintuc.vn/uploads/medias/2016/12/15/150x100_crop/truong-phong-nong-nghiep-gay-tai-nan-3-nguoi-thuong-vong-bb-baaabOKoMA.jpg"></div>
+                                <div class="date-title-desc">
+                                    <div class="n-date">15/12/2016 19:55 - Giao thông</div>
+                                    <div class="n-title"><a>Trưởng phòng nông nghiệp gây tai nạn, 3 người thương vong</a></div>
+                                    <div class="n-desc">Chiều nay, Trung tá Nguyễn Tiến Dũng, Phó trưởng Công an huyện Quảng Xương (Thanh Hóa) xác nhận, vụ tai nạn do Trưởng phòng nông...</div>
+                                </div>
+                            </div>
+                            <div class="news-item">
+                                <div class="n-img"><img src="http://media.tintuc.vn/uploads/medias/2016/12/15/150x100_crop/1-bb-baaad4uhUn.jpg"></div>
+                                <div class="date-title-desc">
+                                    <div class="n-date">15/12/2016 19:55 - Giao thông</div>
+                                    <div class="n-title"><a>Trưởng phòng nông nghiệp gây tai nạn, 3 người thương vong</a></div>
+                                    <div class="n-desc">Chiều nay, Trung tá Nguyễn Tiến Dũng, Phó trưởng Công an huyện Quảng Xương (Thanh Hóa) xác nhận, vụ tai nạn do Trưởng phòng nông...</div>
+                                </div>
+                            </div>
+                            <div class="news-item">
+                                <div class="n-img"><img src="http://media.tintuc.vn/uploads/medias/2016/12/15/150x100_crop/loc-1-1481717794441-1481773520441-bb-baaadlLGQt.jpg"></div>
+                                <div class="date-title-desc">
+                                    <div class="n-date">15/12/2016 19:55 - Giao thông</div>
+                                    <div class="n-title"><a>Trưởng phòng nông nghiệp gây tai nạn, 3 người thương vong</a></div>
+                                    <div class="n-desc">Chiều nay, Trung tá Nguyễn Tiến Dũng, Phó trưởng Công an huyện Quảng Xương (Thanh Hóa) xác nhận, vụ tai nạn do Trưởng phòng nông...</div>
+                                </div>
+                            </div>
+                            <div class="news-item">
+                                <div class="n-img"><img src="http://media.tintuc.vn/uploads/medias/2016/12/15/150x100_crop/ben-tre-nhieu-cong-nhan-nhap-vien-nghi-do-ngo-doc-thuc-pham-bb-baaadjHqdH.jpg"></div>
+                                <div class="date-title-desc">
+                                    <div class="n-date">15/12/2016 19:55 - Giao thông</div>
+                                    <div class="n-title"><a>Trưởng phòng nông nghiệp gây tai nạn, 3 người thương vong</a></div>
+                                    <div class="n-desc">Chiều nay, Trung tá Nguyễn Tiến Dũng, Phó trưởng Công an huyện Quảng Xương (Thanh Hóa) xác nhận, vụ tai nạn do Trưởng phòng nông...</div>
+                                </div>
+                            </div>
+                            <div class="news-item">
+                                <div class="n-img"><img src="http://media.tintuc.vn/uploads/medias/2016/12/15/150x100_crop/bat-duoc-thuy-quai-song-mekong-nang-230kg-bb-baaacA3mth.jpg"></div>
+                                <div class="date-title-desc">
+                                    <div class="n-date">15/12/2016 19:55 - Giao thông</div>
+                                    <div class="n-title"><a>Trưởng phòng nông nghiệp gây tai nạn, 3 người thương vong</a></div>
+                                    <div class="n-desc">Chiều nay, Trung tá Nguyễn Tiến Dũng, Phó trưởng Công an huyện Quảng Xương (Thanh Hóa) xác nhận, vụ tai nạn do Trưởng phòng nông...</div>
+                                </div>
+                            </div>
+                            <div class="news-item">
+                                <div class="n-img"><img src="http://media.tintuc.vn/uploads/medias/2016/12/15/150x100_crop/cong-an-quan-doi-cung-dieu-tra-vu-xe-cho-pham-nhan-gap-nan-bb-baaad9PRL3.jpg"></div>
+                                <div class="date-title-desc">
+                                    <div class="n-date">15/12/2016 19:55 - Giao thông</div>
+                                    <div class="n-title"><a>Trưởng phòng nông nghiệp gây tai nạn, 3 người thương vong</a></div>
+                                    <div class="n-desc">Chiều nay, Trung tá Nguyễn Tiến Dũng, Phó trưởng Công an huyện Quảng Xương (Thanh Hóa) xác nhận, vụ tai nạn do Trưởng phòng nông...</div>
+                                </div>
+                            </div>
+                            <div class="news-item">
+                                <div class="n-img"><img src="http://media.tintuc.vn/uploads/medias/2016/12/15/150x100_crop/ha-tinh-giam-doc-so-tnampmt-bi-quay-nhu-chong-chong-vi-chuyen-moi-truong-bb-baaacGBwEW.jpg"></div>
+                                <div class="date-title-desc">
+                                    <div class="n-date">15/12/2016 19:55 - Giao thông</div>
+                                    <div class="n-title"><a>Trưởng phòng nông nghiệp gây tai nạn, 3 người thương vong</a></div>
+                                    <div class="n-desc">Chiều nay, Trung tá Nguyễn Tiến Dũng, Phó trưởng Công an huyện Quảng Xương (Thanh Hóa) xác nhận, vụ tai nạn do Trưởng phòng nông...</div>
+                                </div>
+                            </div>
+
+                            <div style="margin-top: 20px">
+                                <div class="box-fanpage">
+                                    <iframe name="f23a86b528063" width="490px" height="130px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:page Facebook Social Plugin" src="https://www.facebook.com/v2.3/plugins/page.php?app_id=602777583182431&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2FP5DLcu0KGJB.js%3Fversion%3D42%23cb%3Df15e9d35af8fd44%26domain%3Dtintuc.vn%26origin%3Dhttp%253A%252F%252Ftintuc.vn%252Ff3aefba1bec2028%26relation%3Dparent.parent&amp;container_width=490&amp;height=130&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fm.tintuc.vn&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=false&amp;width=490" style="border: none; visibility: visible; width: 490px; height: 130px;" class=""></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- content-left -->
+                <div class="content-right">
+                    <div class="ads-300-250">
+                        <img src="http://bcadz.com/wp-content/uploads/2015/09/300x250.jpg" />
+                    </div>
+                    <div class="ads-300-250">
+                        <img src="http://www2.jersey.com/business/PublishingImages/marketing/google/300-x-250-An-Hour-Away.jpg" />
+                    </div>
+                    <div class="archire-skin-sibar">
+                        <p class="caption-skin1"><span>Fanpage</span></p>
+                        <div class="box-fanpage">
+                            <iframe name="f23a86b528063" width="300px" height="400px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:page Facebook Social Plugin" src="https://www.facebook.com/v2.3/plugins/page.php?app_id=602777583182431&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2FP5DLcu0KGJB.js%3Fversion%3D42%23cb%3Df15e9d35af8fd44%26domain%3Dtintuc.vn%26origin%3Dhttp%253A%252F%252Ftintuc.vn%252Ff3aefba1bec2028%26relation%3Dparent.parent&amp;container_width=300&amp;height=400&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fm.tintuc.vn&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=false&amp;width=300" style="border: none; visibility: visible; width: 300px; height: 230px;" class=""></iframe>
+                        </div>
+                    </div>
+                    <div class="ads-300-250">
+                        <img src="http://cache.johnchow.com/wp-content/uploads/2006/10/Google-ChromeScreenSnapz005.jpg" />
+                    </div>
+                </div>
+            </div>
+        <?php
+            // require '_include/archire_skin1.php';
+            // require '_include/slider.php';
+            // require '_include/archire_skin2.php';
+            // require '_include/archire_skin3.php';
+            // require '_include/archire_skin4.php';
+            // require '_include/archire_skin5.php';
+            require '_include/slider2.php';
+            require '_include/footernav.php';
+            require '_include/footer.php';
+        ?>
+        <script src="js/all.js" type="text/javascript"></script>
+    </body>
+</html>
