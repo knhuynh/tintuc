613d7db102d359898ff47f81c588191f3a80df6d
Kim Ngoc Huynh <knhuynhtctv@gmail.com>
2016-12-18 17:57:46 +0700

    [-] Code 18/12/2016 : Hoan thien layout cho trang detail. (phan 2)

--- detail.php
+++ detail.php
@@ -21,18 +21,122 @@
                     <div class="news-path">
                         <a>Trang chủ</a> / <a>Xã hội</a> / Cháy nhà ở sài gòn, 6 người tử vong...
                     </div>
-                    
-                    <div class="news-latest">
-                        <div class="news-latest-right">
+                    <div class="detail-title">Thành viên phi hành đoàn tiết lộ lý do thoát chết thần kỳ trong vụ rơi máy bay ở Colombia</div>
+                    <div class="date-like-share">08:40 ngày 01/12/2016</div>
+                    <div class="news-latest news-latest2">
+                        <div class="content-detail-left">
+                            <div class="block-read-more">
+                                <div class="rm-title">
+                                    <div class="rm-title-left">Đọc nhiều</div>
+                                    <div class="rm-title-right"></div>
+                                    <div class="clear"></div>
+                                </div>
+                                <div class="news-featured">
+                                    <a>
+                                        <img src="http://media.tintuc.vn/uploads/medias/2016/12/17/150x100_crop/me-con-toi-biet-song-sao-voi-du-luan-sau-toi-ac-cua-anh-bb-baaacw3ah1.jpg" />
+                                        <div>Vợ kẻ sát hại 4 bà cháu ở Quảng Ninh: 'Mẹ con tôi biết sống sao với dư luận sau tội ác của anh'</div>
+                                    </a>
+                                </div>
+                                <div class="news-2">
+                                    <a>Thừa Thiên-Huế: Hàng trăm người đội mưa đợi tin học sinh bị lũ cuốn</a>
+                                </div>
+                                <div class="news-2">
+                                    <a>Thừa Thiên-Huế: Hàng trăm người đội mưa đợi tin học sinh bị lũ cuốn</a>
+                                </div>
+                                <div class="news-2">
+                                    <a>Thừa Thiên-Huế: Hàng trăm người đội mưa đợi tin học sinh bị lũ cuốn</a>
+                                </div>
+                                <div class="news-2">
+                                    <a>Thừa Thiên-Huế: Hàng trăm người đội mưa đợi tin học sinh bị lũ cuốn</a>
+                                </div>
+                                <div class="news-2">
+                                    <a>Thừa Thiên-Huế: Hàng trăm người đội mưa đợi tin học sinh bị lũ cuốn</a>
+                                </div>
+                                <div class="news-2">
+                                    <a>Thừa Thiên-Huế: Hàng trăm người đội mưa đợi tin học sinh bị lũ cuốn</a>
+                                </div>
+                                <div class="news-2">
+                                    <a>Thừa Thiên-Huế: Hàng trăm người đội mưa đợi tin học sinh bị lũ cuốn</a>
+                                </div>
+                            </div>
                             <img src="https://s-media-cache-ak0.pinimg.com/originals/5f/8d/9c/5f8d9c48f0a9079616b3ef1ee2fe299a.jpg">
                         </div>
-                        <div class="news-latest-left">
-                            
+                        <div class="content-detail-right">
+                            <div class="content-detail-description">
+                                Vụ Trung Quốc bắt giữ tàu lặn không người lái (UUV) của Mỹ đánh dấu nấc leo thang mới trong những nỗ lực ngăn cản các hoạt động do thám hải quân của Washington từ phía Bắc Kinh.
+                            </div>
+                            <ul class="relate-news-content">
+                                <li><a>Thu tàu lặn của Mỹ ở Biển Đông, Trung Quốc phát tín hiệu gì?</a></li>
+                            </ul>
+                            <div class="content-detail">
+                                <img src="http://media.baotintuc.vn/2016/12/17/14/46/004.jpg" />
+                                <p>Tờ "Wall Street Journal" có bài viết đưa ra quan điểm trên. Bên cạnh đó, vụ việc này có thể phản ánh những quan ngại ngày càng tăng của Trung Quốc trước việc Mỹ theo dõi tàu ngầm của Bắc Kinh.</p><br>
+
+                                <p>Chuyên gia về an ninh hàng hải thuộc Viện Công nghệ Massachusetts, ông M. Taylor Fravel cho rằng loại tàu lặn này có thể giúp Hải quân Mỹ theo dõi tàu ngầm của Trung Quốc, do đó có thể có liên quan trực tiếp đến cuộc chiến chống tàu ngầm.</p><br>
+
+                                <p>Trong những năm gần đây, Bắc Kinh đã nhanh chóng mở rộng hạm đội tàu ngầm, khiến Washington phải tăng cường các nỗ lực do thám, trong khi Trung Quốc tìm cách lẩn trốn các hệ thống phát hiện tàu ngầm của Mỹ.</p><br>
+
+                                <p>Theo nhiều quan chức Mỹ, các tàu ngầm tấn công của Trung Quốc được triển khai sâu vào hai vùng biển Ấn Độ Dương và Thái Bình Dương. Các quan chức này dự đoán Bắc Kinh sẽ sớm bắt đầu tiến hành tuần tra bằng tàu ngầm có trang bị tên lửa hạt nhân, nhiều khả năng nhất là tại Biển Đông.</p><br>
+
+                                <p>Trong khi đó, giám đốc Chương trình an ninh quốc tế thuộc Viện Lowy ở Sydney (Australia), ông Euan Graham cho rằng mục đích của Trung Quốc trong vụ việc trên là gieo rắc sự bất đồng hơn nữa giữa Mỹ và Tổng thống Philippines Rodrigo Duterte.</p><br>
+
+                                <p>Kể từ khi nhậm chức, ông Duterte đã lảng tránh Washington và tỏ ý muốn xích lại gần hơn với Bắc Kinh. Thời gian qua, các quan chức Philippines bớt chỉ trích những hoạt động của Bắc Kinh trên Biển Đông, mặc dù tuần qua một số người đã bày tỏ quan ngại sau khi một cơ quan tư vấn của Mỹ cung cấp nhiều hình ảnh chụp vệ tinh cho thấy Bắc Kinh đã triển khai máy bay và một số vũ khí khác trên các đảo nhân tạo trên Biển Đông.</p>
+                            </div>
+                            <div class="content-contact">
+                                Bạn đang đọc bài viết <a>Thu tàu lặn của Mỹ ở Biển Đông, Trung Quốc phát tín hiệu gì?</a> tại chuyên mục <a>Bình luận quốc tế</a> của trang <a>Tin Tức</a> Việt Nam. Mọi thông tin góp ý và chia sẻ, xin vui lòng gửi về hòm thư <a>bbt@tintuc.vn</a>
+                            </div>
+
+                            <div style="margin-top: 20px">
+                                <div class="box-fanpage">
+                                    <iframe name="f23a86b528063" width="490px" height="130px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:page Facebook Social Plugin" src="https://www.facebook.com/v2.3/plugins/page.php?app_id=602777583182431&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2FP5DLcu0KGJB.js%3Fversion%3D42%23cb%3Df15e9d35af8fd44%26domain%3Dtintuc.vn%26origin%3Dhttp%253A%252F%252Ftintuc.vn%252Ff3aefba1bec2028%26relation%3Dparent.parent&amp;container_width=490&amp;height=130&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fm.tintuc.vn&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=false&amp;width=490" style="border: none; visibility: visible; width: 490px; height: 130px;" class=""></iframe>
+                                </div>
+                            </div>
+
                         </div>
                         <div class="clear"></div>
-                        <div class="post-featured-level2">
+                        <div class="tags">Từ khóa:
+                            <a>rắn kỳ quái</a>
+                            <a>Rắn hai đầu</a>
+                            <a>Rắn ngàn năm</a>
+                            <a>chuyện kỳ quái</a>
+                        </div>
+                        <div class="post-featured-relate">
                             <div class="relate-title">Có thể bạn quan tâm<span class="line-middle"></span></div>
                             <div class="post-item">
+                                <a><img src="http://media.tintuc.vn/uploads/medias/2016/12/13/150x100_crop/cau-duoc-ca-lang-duoi-do-gan-45-kg-tren-song-serepok-bb-baaabMq1PC.jpg" width="154" height="100" />
+                                <span>KHÔNG THỂ TIN: Cướp túi xách, sau 2 tiếng quay lại 'trả nè,</span></a>
+                            </div>
+                            <div class="post-item">
+                                <a><img src="http://media.tintuc.vn/uploads/medias/2016/12/13/150x100_crop/nu-nhan-vien-bi-nhom-thanh-nien-hanh-hung-quyt-tien-bb-baaabRjfgq.jpg" width="154" height="100" />
+                                <span>Đang quan hệ tình dục thì bị 'gãy súng' phải đi cấp cứu</span></a>
+                            </div>
+                            <div class="post-item">
+                                <a><img src="http://media.tintuc.vn/uploads/medias/2016/12/13/150x100_crop/nha-trang-muong-thoat-lu-vo-da-tang-ngon-ngang-trong-nha-dan-bb-baaadDzf4L.jpg" width="154" height="100" />
+                                <span>Xót xa người mẹ trẻ quyết định bán tim để gom đủ 600 triệu đồng</span></a>
+                            </div>
+                            <div class="post-item">
+                                <a><img src="http://media.tintuc.vn/uploads/medias/2016/12/13/150x100_crop/dinh-chi-tai-xe-ban-kinh-te-trung-uong-gay-tai-nan-bo-chay-bb-baaadcyvwp.jpg" width="154" height="100" />
+                                <span>Cảnh sát giao thông bế trẻ qua đoạn nước lụt</span></a>
+                            </div>
+
+                            <div class="post-item">
+                                <a><img src="http://media.tintuc.vn/uploads/medias/2016/12/13/150x100_crop/cau-duoc-ca-lang-duoi-do-gan-45-kg-tren-song-serepok-bb-baaabMq1PC.jpg" width="154" height="100" />
+                                <span>KHÔNG THỂ TIN: Cướp túi xách, sau 2 tiếng quay lại 'trả nè,</span></a>
+                            </div>
+                            <div class="post-item">
+                                <a><img src="http://media.tintuc.vn/uploads/medias/2016/12/13/150x100_crop/nu-nhan-vien-bi-nhom-thanh-nien-hanh-hung-quyt-tien-bb-baaabRjfgq.jpg" width="154" height="100" />
+                                <span>Đang quan hệ tình dục thì bị 'gãy súng' phải đi cấp cứu</span></a>
+                            </div>
+                            <div class="post-item">
+                                <a><img src="http://media.tintuc.vn/uploads/medias/2016/12/13/150x100_crop/nha-trang-muong-thoat-lu-vo-da-tang-ngon-ngang-trong-nha-dan-bb-baaadDzf4L.jpg" width="154" height="100" />
+                                <span>Xót xa người mẹ trẻ quyết định bán tim để gom đủ 600 triệu đồng</span></a>
+                            </div>
+                            <div class="post-item">
+                                <a><img src="http://media.tintuc.vn/uploads/medias/2016/12/13/150x100_crop/dinh-chi-tai-xe-ban-kinh-te-trung-uong-gay-tai-nan-bo-chay-bb-baaadcyvwp.jpg" width="154" height="100" />
+                                <span>Cảnh sát giao thông bế trẻ qua đoạn nước lụt</span></a>
+                            </div>
+
+                             <div class="post-item">
                                 <a><img src="http://media.tintuc.vn/uploads/medias/2016/12/13/150x100_crop/cau-duoc-ca-lang-duoi-do-gan-45-kg-tren-song-serepok-bb-baaabMq1PC.jpg" width="154" height="100" />
                                 <span>KHÔNG THỂ TIN: Cướp túi xách, sau 2 tiếng quay lại 'trả nè,</span></a>
                             </div>
