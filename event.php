363360402bd612f401d1f02e24de0417145b0c3b
Kim Ngoc Huynh <knhuynhtctv@gmail.com>
2016-12-22 22:15:50 +0700

    [+] Code 22/12/2016 : Add event page.

---
+++ event.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Xã Hội, Tin Tức Xã Hội</title>
+        <link href="css/all.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <?php
+            require '_include/topnav.php';
+            require '_include/banner.php';
+            // require '_include/mainnav.php';
+            // require '_include/topevent2.php';
+        ?>
+            <div class="wraper">
+                <div style="border-top: 3px solid #d52025"></div>
+                <div class="content-left">
+                    <div class="news-path">
+                        <a>Trang chủ</a> / <a>Xã hội</a> / Cháy nhà ở sài gòn, 6 người tử vong...
+                    </div>
+                    <ul class="event-years">
+                        <li><a>2015</a></li>
+                        <li><a>2016</a></li>
+                        <li><a>2017</a></li>
+                    </ul>
+                </div> <!-- content-left -->
+                <div class="content-right">
+                    <div class="ads-300-250">
+                        <img src="http://bcadz.com/wp-content/uploads/2015/09/300x250.jpg" />
+                    </div>
+                    <div class="ads-300-250">
+                        <img src="http://www2.jersey.com/business/PublishingImages/marketing/google/300-x-250-An-Hour-Away.jpg" />
+                    </div>
+                    <div class="archire-skin-sibar">
+                        <p class="caption-skin1"><span>Fanpage</span></p>
+                        <div class="box-fanpage">
+                            <iframe name="f23a86b528063" width="300px" height="400px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:page Facebook Social Plugin" src="https://www.facebook.com/v2.3/plugins/page.php?app_id=602777583182431&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2FP5DLcu0KGJB.js%3Fversion%3D42%23cb%3Df15e9d35af8fd44%26domain%3Dtintuc.vn%26origin%3Dhttp%253A%252F%252Ftintuc.vn%252Ff3aefba1bec2028%26relation%3Dparent.parent&amp;container_width=300&amp;height=400&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fm.tintuc.vn&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=false&amp;width=300" style="border: none; visibility: visible; width: 300px; height: 230px;" class=""></iframe>
+                        </div>
+                    </div>
+                    <div class="ads-300-250">
+                        <img src="http://cache.johnchow.com/wp-content/uploads/2006/10/Google-ChromeScreenSnapz005.jpg" />
+                    </div>
+                </div>
+            </div>
+        <?php
+            // require '_include/archire_skin1.php';
+            // require '_include/slider.php';
+            // require '_include/archire_skin2.php';
+            // require '_include/archire_skin3.php';
+            // require '_include/archire_skin4.php';
+            // require '_include/archire_skin5.php';
+            require '_include/slider2.php';
+            require '_include/footernav.php';
+            require '_include/footer.php';
+        ?>
+        <script src="js/all.js" type="text/javascript"></script>
+    </body>
+</html>
